6ed0d945d77421ac53732a2c8b9df720fe68657d
unnamed <>
2020-12-09 13:15:40 +0100

    :adhesive_bandage: Fix addig units to number
    Fixes output for the temp value equal to 0

--- src/utils/units.ts
+++ src/utils/units.ts
@@ -1,6 +1,6 @@
 export const addUnits = (temp: number, units = 'ºC') => {
 	const celsius = temp; // - 273.15;
-	const unitMark = units;
-	const round = celsius < 10 && celsius > -10 ? 1 : 0;
-	return `${celsius.toFixed(round)}${unitMark}`;
+	const unitSymbol = units;
+	const round = celsius === 0 ? 0 : celsius < 10 && celsius > -10 ? 1 : 0;
+	return `${celsius.toFixed(round)}${unitSymbol}`;
 };
